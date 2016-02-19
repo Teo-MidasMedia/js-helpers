26447ceeea12e798d14f4b481f23447c6b0e9daf
teodora-n <t.nikolaeva84@gmail.com>
2016-02-19 09:20:44 +0000

    Create calculate-paddings-runtime.js

---
+++ calculate-paddings-runtime.js
@@ -0,0 +1,11 @@
+//Calculate paddings at runtime
+function calculatePercentage(el, target, percentage){
+    var elem = $(el);
+    var targetWidth = $(target).outerWidth();
+
+    elem.css({
+       'padding-left': (targetWidth / 100) *percentage,
+       'padding-right': (targetWidth / 100) *percentage 
+    });
+}
+calculatePercentage('.u-pad-1-12', '.site-wrap', 8.3333);
